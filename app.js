eef1b3ab267c5887f48e9b2f3e5083fbebec7daa
KAYAJAGANNATH-88 <kayajagannath119@gmail.com>
2025-11-01 19:15:07 +0530

    folder-1 done

---
+++ app.js
@@ -0,0 +1,6 @@
+//var a=20;
+//export default a;
+const name="kaya";
+export var age=21;
+export const city="banglore";
+export default name; 
---
+++ script.js
@@ -0,0 +1,14 @@
+/*var h1=document.createElement('h1');
+h1.innerHTML='Hello react';
+console.log(h1)
+document.body.appendChild(h1);*/
+
+//import a from './app.js';
+//console.log(a);
+
+/*import name from './app.js';
+console.log(name);
+import {city,age} from './app.js';
+console.log(city,age);*/
+
+ 
